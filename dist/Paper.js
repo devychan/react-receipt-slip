e7fd2425f17037b6984b0fa17f74288a38928d02
devychan <keuriseutian15@gmail.com>
2025-07-12 12:19:39 +0800

    v1.0.4 - fixed deps

--- dist/Paper.js
+++ dist/Paper.js
@@ -1,66 +1,101 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TableCell = exports.TableHead = exports.TableRow = exports.DataTable = exports.RowText = exports.Dot = exports.Line = exports.Text = exports.Paper = void 0;
-const styled_components_1 = __importDefault(require("styled-components"));
-exports.Paper = styled_components_1.default.div `
-    max-width: ${(props) => { var _a; return (_a = props.size) !== null && _a !== void 0 ? _a : 80; }}mm;
+// src/Paper.tsx
+import styled from "styled-components";
+var Paper = styled.div`
+    max-width: ${(props) => {
+  var _a;
+  return (_a = props.size) != null ? _a : 80;
+}}mm;
     width: 100%;
 `;
-exports.Text = styled_components_1.default.div `
-    text-align: ${(props) => { var _a; return (_a = props.align) !== null && _a !== void 0 ? _a : 'left'; }};
-    font-weight: ${(props) => props.bold ? 'bold' : 'normal'};
-    text-decoration: ${(props) => props.underlined ? 'underline' : 'unset'};
+var Text = styled.div`
+    text-align: ${(props) => {
+  var _a;
+  return (_a = props.align) != null ? _a : "left";
+}};
+    font-weight: ${(props) => props.bold ? "bold" : "normal"};
+    text-decoration: ${(props) => props.underlined ? "underline" : "unset"};
     font-family: monospace;
 `;
-exports.Line = styled_components_1.default.div `
+var Line = styled.div`
     border: 0.5px dashed gray;
     width: 100%;
     height: 0px;
     margin-bottom: 5px;
 `;
-exports.Dot = styled_components_1.default.hr `
-    border: 0.5px ${(props) => { var _a; return (_a = props.type) !== null && _a !== void 0 ? _a : 'dashed'; }} gray;
+var Dot = styled.hr`
+    border: 0.5px ${(props) => {
+  var _a;
+  return (_a = props.type) != null ? _a : "dashed";
+}} gray;
     margin-top: ${(props) => props.vmar ? props.vmar[0] : 0}px;
     margin-bottom: ${(props) => props.vmar ? props.vmar[1] : 0}px;
 `;
-exports.RowText = styled_components_1.default.div `
+var RowText = styled.div`
     display: flex;
     align-items: ${(props) => {
-    if (props.valign === "bottom") {
-        return 'flex-end';
-    }
-    else if (props.valign === "top") {
-        return 'flex-start';
-    }
-    else {
-        return 'center';
-    }
+  if (props.valign === "bottom") {
+    return "flex-end";
+  } else if (props.valign === "top") {
+    return "flex-start";
+  } else {
+    return "center";
+  }
 }};
     justify-content: space-between;
-    gap: ${(props) => { var _a; return (_a = props.gap) !== null && _a !== void 0 ? _a : 0; }}px;
-    padding-top: ${(props) => { var _a; return (_a = props.pady) !== null && _a !== void 0 ? _a : 0; }}px;
-    padding-right: ${(props) => { var _a; return (_a = props.padx) !== null && _a !== void 0 ? _a : 0; }}px;
-    padding-bottom: ${(props) => { var _a; return (_a = props.pady) !== null && _a !== void 0 ? _a : 0; }}px;
-    padding-left: ${(props) => { var _a; return (_a = props.padx) !== null && _a !== void 0 ? _a : 0; }}px;
+    gap: ${(props) => {
+  var _a;
+  return (_a = props.gap) != null ? _a : 0;
+}}px;
+    padding-top: ${(props) => {
+  var _a;
+  return (_a = props.pady) != null ? _a : 0;
+}}px;
+    padding-right: ${(props) => {
+  var _a;
+  return (_a = props.padx) != null ? _a : 0;
+}}px;
+    padding-bottom: ${(props) => {
+  var _a;
+  return (_a = props.pady) != null ? _a : 0;
+}}px;
+    padding-left: ${(props) => {
+  var _a;
+  return (_a = props.padx) != null ? _a : 0;
+}}px;
 `;
-exports.DataTable = styled_components_1.default.table `
+var DataTable = styled.table`
     border-collapse: collapse;
     width: 100%;
 `;
-exports.TableRow = styled_components_1.default.tr `
-    text-align: ${(props) => { var _a; return (_a = props.align) !== null && _a !== void 0 ? _a : 'left'; }};
+var TableRow = styled.tr`
+    text-align: ${(props) => {
+  var _a;
+  return (_a = props.align) != null ? _a : "left";
+}};
 `;
-exports.TableHead = styled_components_1.default.th `
+var TableHead = styled.th`
     border: 1px dashed gray;
     border-left-width: 0px;
     border-right-width: 0px;
     font-family: monospace;
     padding: 0px 5px;
 `;
-exports.TableCell = styled_components_1.default.td `
-    text-align: ${(props) => { var _a; return (_a = props.align) !== null && _a !== void 0 ? _a : 'left'; }};
+var TableCell = styled.td`
+    text-align: ${(props) => {
+  var _a;
+  return (_a = props.align) != null ? _a : "left";
+}};
     font-family: monospace;
 `;
+export {
+  DataTable,
+  Dot,
+  Line,
+  Paper,
+  RowText,
+  TableCell,
+  TableHead,
+  TableRow,
+  Text
+};
+//# sourceMappingURL=Paper.js.map
